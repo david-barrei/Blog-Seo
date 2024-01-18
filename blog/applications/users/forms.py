244fa4670a250f093d61ca50dbdcb580b0807204
david-barrei <davbarreiro4@gmail.com>
2024-01-18 01:34:53 -0500

    Registro de usuario

--- blog/applications/users/forms.py
+++ blog/applications/users/forms.py
@@ -35,6 +35,18 @@ class UserRegisterForm(forms.ModelForm):
             'genero',
             'date_birth',
         )
+        widgets= {
+            'email': forms.EmailInput(
+                attrs={
+                    'placeholder':'Correo Electronico..'
+                }
+            ),
+            'date_birth': forms.DateInput(
+                attrs={
+                    'type':'date'
+                }
+            ),
+        }
     
     def clean_password2(self):
         if self.cleaned_data['password1'] != self.cleaned_data['password2']:
